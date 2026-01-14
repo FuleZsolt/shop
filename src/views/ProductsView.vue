f217f4d1e566d19058825cfbabdc625dd1f394b9
Zsolt <sze4fulzso@vasvari.org>
2026-01-14 17:14:20 +0100

    _1

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,5 +1,16 @@
 <script setup>
+import axios from 'axios';
+import { ref, onMounted } from 'vue'
+import {toast} from 'vue3-toastify';
 
+const products = ref([])
+
+onMounted(() => {
+ // toast.info('Termékek letöltve!')
+  axios.get('http://localhost:3000/products')
+  .then(resp => products.value = resp.data)
+  .then (toast.info('Termékek letöltve!'))
+})
 </script>
 
 <template>
